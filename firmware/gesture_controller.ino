b712612738ec881f471df21b0a7c65988219055d
Benny Pratama <138679489+bennyprtma@users.noreply.github.com>
2026-08-30 14:38:54 +0700

    Update gesture_controller.ino

--- firmware/gesture_controller.ino
+++ firmware/gesture_controller.ino
@@ -1,1 +1,654 @@
+#include <GestureSpotifyControl_inferencing.h>
+#include "edge-impulse-sdk/dsp/image/image.hpp"
+#include "esp_camera.h"
+#include "WiFi.h"
+#include "HTTPClient.h"
+#include "ArduinoJson.h"
 
+const char* ssid = "YOUR_WIFI_SSID";
+const char* password = "YOUR_WIFI_PASSWORD";
+const char* serverAddress = "YOUR_PC_IP";
+const int serverPort = 5000;
+
+#define CAMERA_MODEL_AI_THINKER
+
+#if defined(CAMERA_MODEL_ESP_EYE)
+#define PWDN_GPIO_NUM -1
+#define RESET_GPIO_NUM -1
+#define XCLK_GPIO_NUM 4
+#define SIOD_GPIO_NUM 18
+#define SIOC_GPIO_NUM 23
+#define Y9_GPIO_NUM 36
+#define Y8_GPIO_NUM 37
+#define Y7_GPIO_NUM 38
+#define Y6_GPIO_NUM 39
+#define Y5_GPIO_NUM 35
+#define Y4_GPIO_NUM 14
+#define Y3_GPIO_NUM 13
+#define Y2_GPIO_NUM 34
+#define VSYNC_GPIO_NUM 5
+#define HREF_GPIO_NUM 27
+#define PCLK_GPIO_NUM 25
+
+#elif defined(CAMERA_MODEL_AI_THINKER)
+#define PWDN_GPIO_NUM 32
+#define RESET_GPIO_NUM -1
+#define XCLK_GPIO_NUM 0
+#define SIOD_GPIO_NUM 26
+#define SIOC_GPIO_NUM 27
+#define Y9_GPIO_NUM 35
+#define Y8_GPIO_NUM 34
+#define Y7_GPIO_NUM 39
+#define Y6_GPIO_NUM 36
+#define Y5_GPIO_NUM 21
+#define Y4_GPIO_NUM 19
+#define Y3_GPIO_NUM 18
+#define Y2_GPIO_NUM 5
+#define VSYNC_GPIO_NUM 25
+#define HREF_GPIO_NUM 23
+#define PCLK_GPIO_NUM 22
+
+#else
+#error "Camera model not selected"
+#endif
+
+#define EI_CAMERA_RAW_FRAME_BUFFER_COLS 320
+#define EI_CAMERA_RAW_FRAME_BUFFER_ROWS 240
+#define EI_CAMERA_FRAME_BYTE_SIZE 3
+
+static bool debug_nn = false;
+static bool is_initialised = false;
+uint8_t *snapshot_buf;
+
+static camera_config_t camera_config = {
+    .pin_pwdn = PWDN_GPIO_NUM,
+    .pin_reset = RESET_GPIO_NUM,
+    .pin_xclk = XCLK_GPIO_NUM,
+    .pin_sscb_sda = SIOD_GPIO_NUM,
+    .pin_sscb_scl = SIOC_GPIO_NUM,
+    .pin_d7 = Y9_GPIO_NUM,
+    .pin_d6 = Y8_GPIO_NUM,
+    .pin_d5 = Y7_GPIO_NUM,
+    .pin_d4 = Y6_GPIO_NUM,
+    .pin_d3 = Y5_GPIO_NUM,
+    .pin_d2 = Y4_GPIO_NUM,
+    .pin_d1 = Y3_GPIO_NUM,
+    .pin_d0 = Y2_GPIO_NUM,
+    .pin_vsync = VSYNC_GPIO_NUM,
+    .pin_href = HREF_GPIO_NUM,
+    .pin_pclk = PCLK_GPIO_NUM,
+    .xclk_freq_hz = 20000000,
+    .ledc_timer = LEDC_TIMER_0,
+    .ledc_channel = LEDC_CHANNEL_0,
+    .pixel_format = PIXFORMAT_JPEG,
+    .frame_size = FRAMESIZE_QVGA,
+    .jpeg_quality = 12,
+    .fb_count = 1,
+    .fb_location = CAMERA_FB_IN_PSRAM,
+    .grab_mode = CAMERA_GRAB_WHEN_EMPTY,
+};
+
+void sendGestureToPC(String gesture) {
+    WiFiClient client;
+    HTTPClient http;
+
+    String serverPath =
+        "http://" +
+        String(serverAddress) +
+        ":" +
+        String(serverPort) +
+        "/gesture";
+
+    http.begin(client, serverPath);
+    http.addHeader("Content-Type", "application/json");
+
+    StaticJsonDocument<200> doc;
+    doc["gesture"] = gesture;
+
+    String requestBody;
+    serializeJson(doc, requestBody);
+
+    Serial.print("Sending JSON: ");
+    Serial.println(requestBody);
+
+    int httpResponseCode = http.POST(requestBody);
+
+    if (httpResponseCode > 0) {
+        Serial.printf(
+            "HTTP Response code: %d\n",
+            httpResponseCode
+        );
+
+        String payload = http.getString();
+        Serial.println(
+            "Server Response: " + payload
+        );
+    } else {
+        Serial.printf(
+            "HTTP Error: %s\n",
+            http.errorToString(
+                httpResponseCode
+            ).c_str()
+        );
+    }
+
+    http.end();
+}
+
+bool ei_camera_init(void);
+void ei_camera_deinit(void);
+
+bool ei_camera_capture(
+    uint32_t img_width,
+    uint32_t img_height,
+    uint8_t *out_buf
+);
+
+static int ei_camera_get_data(
+    size_t offset,
+    size_t length,
+    float *out_ptr
+);
+
+void setup() {
+    Serial.begin(115200);
+
+    while (!Serial);
+
+    Serial.println(
+        "Edge Impulse Inferencing Demo"
+    );
+
+    Serial.print(
+        "Connecting to WiFi: "
+    );
+
+    Serial.println(ssid);
+
+    WiFi.begin(
+        ssid,
+        password
+    );
+
+    while (
+        WiFi.status() != WL_CONNECTED
+    ) {
+        delay(500);
+        Serial.print(".");
+    }
+
+    Serial.println(
+        "\nWiFi connected!"
+    );
+
+    Serial.print(
+        "IP address: "
+    );
+
+    Serial.println(
+        WiFi.localIP()
+    );
+
+    if (!ei_camera_init()) {
+        ei_printf(
+            "Failed to initialize Camera!\r\n"
+        );
+    } else {
+        ei_printf(
+            "Camera initialized\r\n"
+        );
+    }
+
+    ei_printf(
+        "\nStarting continuous inference in 2 seconds...\n"
+    );
+
+    ei_sleep(2000);
+}
+
+void loop() {
+    if (
+        ei_sleep(5) != EI_IMPULSE_OK
+    ) {
+        return;
+    }
+
+    snapshot_buf =
+        (uint8_t*)malloc(
+            EI_CAMERA_RAW_FRAME_BUFFER_COLS *
+            EI_CAMERA_RAW_FRAME_BUFFER_ROWS *
+            EI_CAMERA_FRAME_BYTE_SIZE
+        );
+
+    if (
+        snapshot_buf == nullptr
+    ) {
+        ei_printf(
+            "ERR: Failed to allocate snapshot buffer!\n"
+        );
+
+        return;
+    }
+
+    ei::signal_t signal;
+
+    signal.total_length =
+        EI_CLASSIFIER_INPUT_WIDTH *
+        EI_CLASSIFIER_INPUT_HEIGHT;
+
+    signal.get_data =
+        &ei_camera_get_data;
+
+    if (
+        !ei_camera_capture(
+            EI_CLASSIFIER_INPUT_WIDTH,
+            EI_CLASSIFIER_INPUT_HEIGHT,
+            snapshot_buf
+        )
+    ) {
+        ei_printf(
+            "Failed to capture image\r\n"
+        );
+
+        free(snapshot_buf);
+
+        return;
+    }
+
+    ei_impulse_result_t result = {0};
+
+    EI_IMPULSE_ERROR err =
+        run_classifier(
+            &signal,
+            &result,
+            debug_nn
+        );
+
+    if (
+        err != EI_IMPULSE_OK
+    ) {
+        ei_printf(
+            "ERR: Failed to run classifier (%d)\n",
+            err
+        );
+
+        free(snapshot_buf);
+
+        return;
+    }
+
+#if EI_CLASSIFIER_OBJECT_DETECTION == 1
+
+    float max_confidence = 0.0;
+    String detected_gesture = "none";
+
+    const float confidence_threshold =
+        0.50;
+
+    for (
+        uint32_t i = 0;
+        i < result.bounding_boxes_count;
+        i++
+    ) {
+        ei_impulse_result_bounding_box_t bb =
+            result.bounding_boxes[i];
+
+        if (
+            bb.value == 0
+        ) {
+            continue;
+        }
+
+        Serial.printf(
+            "Detected: '%s' with confidence: %.5f\n",
+            bb.label,
+            bb.value
+        );
+
+        if (
+            bb.value > max_confidence
+        ) {
+            max_confidence =
+                bb.value;
+
+            detected_gesture =
+                String(bb.label);
+        }
+    }
+
+    if (
+        max_confidence >
+        confidence_threshold
+    ) {
+        if (
+            detected_gesture == "NEXT"
+        ) {
+            sendGestureToPC(
+                "next"
+            );
+
+            delay(2000);
+        }
+
+        else if (
+            detected_gesture ==
+            "PREVIOUS" ||
+            detected_gesture ==
+            "PREV"
+        ) {
+            sendGestureToPC(
+                "previous"
+            );
+
+            delay(2000);
+        }
+    }
+
+#else
+
+    float max_confidence = 0.0;
+    String detected_gesture = "none";
+
+    for (
+        uint16_t i = 0;
+        i < EI_CLASSIFIER_LABEL_COUNT;
+        i++
+    ) {
+        if (
+            result.classification[i].value >
+            max_confidence
+        ) {
+            max_confidence =
+                result.classification[i].value;
+
+            detected_gesture =
+                result.classification[i].label;
+        }
+    }
+
+    const float confidence_threshold =
+        0.50;
+
+    if (
+        max_confidence >
+        confidence_threshold
+    ) {
+        if (
+            detected_gesture == "NEXT"
+        ) {
+            sendGestureToPC(
+                "next"
+            );
+
+            delay(2000);
+        }
+
+        else if (
+            detected_gesture ==
+            "PREVIOUS"
+        ) {
+            sendGestureToPC(
+                "previous"
+            );
+
+            delay(2000);
+        }
+    }
+
+#endif
+
+#if EI_CLASSIFIER_HAS_ANOMALY
+    ei_printf(
+        "Anomaly prediction: %.3f\r\n",
+        result.anomaly
+    );
+#endif
+
+#if EI_CLASSIFIER_HAS_VISUAL_ANOMALY
+
+    for (
+        uint32_t i = 0;
+        i < result.visual_ad_count;
+        i++
+    ) {
+        ei_impulse_result_bounding_box_t bb =
+            result.visual_ad_grid_cells[i];
+
+        if (
+            bb.value == 0
+        ) {
+            continue;
+        }
+    }
+
+#endif
+
+    free(snapshot_buf);
+}
+
+bool ei_camera_init(void) {
+    if (
+        is_initialised
+    ) {
+        return true;
+    }
+
+#if defined(CAMERA_MODEL_ESP_EYE)
+
+    pinMode(
+        13,
+        INPUT_PULLUP
+    );
+
+    pinMode(
+        14,
+        INPUT_PULLUP
+    );
+
+#endif
+
+    esp_err_t err =
+        esp_camera_init(
+            &camera_config
+        );
+
+    if (
+        err != ESP_OK
+    ) {
+        Serial.printf(
+            "Camera init failed with error 0x%x\n",
+            err
+        );
+
+        return false;
+    }
+
+    sensor_t *s =
+        esp_camera_sensor_get();
+
+    if (
+        s->id.PID ==
+        OV3660_PID
+    ) {
+        s->set_vflip(
+            s,
+            1
+        );
+
+        s->set_brightness(
+            s,
+            1
+        );
+
+        s->set_saturation(
+            s,
+            0
+        );
+    }
+
+#if defined(CAMERA_MODEL_M5STACK_WIDE)
+
+    s->set_vflip(
+        s,
+        1
+    );
+
+    s->set_hmirror(
+        s,
+        1
+    );
+
+#elif defined(CAMERA_MODEL_ESP_EYE)
+
+    s->set_vflip(
+        s,
+        1
+    );
+
+    s->set_hmirror(
+        s,
+        1
+    );
+
+    s->set_awb_gain(
+        s,
+        1
+    );
+
+#endif
+
+    is_initialised = true;
+
+    return true;
+}
+
+void ei_camera_deinit(void) {
+    esp_err_t err =
+        esp_camera_deinit();
+
+    if (
+        err != ESP_OK
+    ) {
+        ei_printf(
+            "Camera deinit failed\n"
+        );
+
+        return;
+    }
+
+    is_initialised = false;
+}
+
+bool ei_camera_capture(
+    uint32_t img_width,
+    uint32_t img_height,
+    uint8_t *out_buf
+) {
+    if (
+        !is_initialised
+    ) {
+        ei_printf(
+            "ERR: Camera is not initialized\r\n"
+        );
+
+        return false;
+    }
+
+    camera_fb_t *fb =
+        esp_camera_fb_get();
+
+    if (
+        !fb
+    ) {
+        ei_printf(
+            "Camera capture failed\n"
+        );
+
+        return false;
+    }
+
+    bool converted =
+        fmt2rgb888(
+            fb->buf,
+            fb->len,
+            PIXFORMAT_JPEG,
+            snapshot_buf
+        );
+
+    esp_camera_fb_return(
+        fb
+    );
+
+    if (
+        !converted
+    ) {
+        ei_printf(
+            "Conversion failed\n"
+        );
+
+        return false;
+    }
+
+    if (
+        img_width !=
+            EI_CAMERA_RAW_FRAME_BUFFER_COLS ||
+        img_height !=
+            EI_CAMERA_RAW_FRAME_BUFFER_ROWS
+    ) {
+        ei::image::processing::
+            crop_and_interpolate_rgb888(
+                out_buf,
+                EI_CAMERA_RAW_FRAME_BUFFER_COLS,
+                EI_CAMERA_RAW_FRAME_BUFFER_ROWS,
+                out_buf,
+                img_width,
+                img_height
+            );
+    }
+
+    return true;
+}
+
+static int ei_camera_get_data(
+    size_t offset,
+    size_t length,
+    float *out_ptr
+) {
+    size_t pixel_ix =
+        offset * 3;
+
+    size_t pixels_left =
+        length;
+
+    size_t out_ptr_ix =
+        0;
+
+    while (
+        pixels_left != 0
+    ) {
+        out_ptr[out_ptr_ix] =
+            (
+                snapshot_buf[
+                    pixel_ix + 2
+                ] << 16
+            ) +
+            (
+                snapshot_buf[
+                    pixel_ix + 1
+                ] << 8
+            ) +
+            snapshot_buf[
+                pixel_ix
+            ];
+
+        out_ptr_ix++;
+        pixel_ix += 3;
+        pixels_left--;
+    }
+
+    return 0;
+}
+
+#if !defined(EI_CLASSIFIER_SENSOR) || EI_CLASSIFIER_SENSOR != EI_CLASSIFIER_SENSOR_CAMERA
+#error "Invalid model for current sensor"
+#endif
